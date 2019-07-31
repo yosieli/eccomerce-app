7badc124ad283de97d157786f6fd0e301f1f563e
Hans Krohn <hanskrohn70@gmail.com>
2019-07-31 10:20:30 -0500

    added stylings and alerts

--- frontend-server/src/components/CartList.js
+++ frontend-server/src/components/CartList.js
@@ -11,7 +11,9 @@ export default  class Cart  extends React.Component {
     state = {
         myItems: [],
         total:0,
-        buyItems: false
+        buyItems: false,
+        chosenItem: {},
+        boughtItem: false,
        
     }
 
@@ -57,11 +59,13 @@ export default  class Cart  extends React.Component {
          })
     }
    
-    handleSubmit = () => {
+    handleSubmit = (item) => {
+        console.log(item)
         this.setState({
-            buyItems: !this.state.buyItems
+            buyItems: !this.state.buyItems,
+            boughtItem: !this.state.boughtItem
         })
-
+       
     }
 
 
@@ -77,9 +81,10 @@ export default  class Cart  extends React.Component {
             return items.id !== item.id
         })
         this.setState({
-         myItems: myItems,
-         total: this.state.total-(item.quantity * item.price),
-         buyItems: !this.state.buyItems
+            chosenItem: item,
+            myItems: myItems,
+            total: this.state.total-(item.quantity * item.price),
+            buyItems: !this.state.buyItems
      })
 
     }
@@ -98,22 +103,29 @@ export default  class Cart  extends React.Component {
         })
        
     } 
-
+    handleBoughtItem = () =>{
+        alert('You just bought: '+ this.state.chosenItem.item_name) 
+        this.setState({
+            boughtItem: !this.state.boughtItem
+        })
+    }
     render(){
      let items = this.state.myItems
      let shown
      console.log(this.state.buyItems)
+    
      if(this.state.buyItems){
         shown = (  <div className="app-container">
                         <div className="row">
                             <div className="col no-gutters">
-                                <CheckOutForm  handleSubmit={this.handleSubmit}/>
+                                <CheckOutForm  handleSubmit={this.handleSubmit} chosenItem = {this.state.chosenItem}/>
                             </div>
                         </div>
                     </div>
                 )
     }
     else{
+       
         shown = (<div style = {{paddingLeft: '6%', "background-color": "rgb(238,236,225)", height: '100vh'}}>
             <h3> My Cart items: ${this.state.total}</h3>
             <h3><Button onClick={() => this.clearCart()}  variant="outline-danger" >Empty Cart</Button> </h3> 
@@ -123,7 +135,7 @@ export default  class Cart  extends React.Component {
                     return(
                     <div>
                         
-                    <CartItems  item = {item}    clearCart={this.clearCart}  removeItem={this.removeItem} byeItems={this.buySpecificItem} /> 
+                    <CartItems  item = {item}    clearCart={this.clearCart}  removeItem={this.removeItem} buyItems={this.buySpecificItem} /> 
                     </div> 
                     )
                 })}
@@ -135,8 +147,8 @@ export default  class Cart  extends React.Component {
     }
      return(
         <div>
-       
             {shown}
+            {this.state.boughtItem ? this.handleBoughtItem() : null }
         </div>
         )
 
--- frontend-server/src/components/CheckOutForm.js
+++ frontend-server/src/components/CheckOutForm.js
@@ -16,7 +16,7 @@ import './CheckOut.css'
             <Input label="CVV" type="number" name="cvv" />
           </div>
         </div>
-        <Button handleSubmit = {props.handleSubmit} text="Place order" />
+        <Button handleSubmit = {props.handleSubmit} chosenItem = {props.chosenItem} text="Place order" />
       </div>
    </div>
   );
@@ -33,7 +33,7 @@ import './CheckOut.css'
   );
   
   const Button = (props) => (
-    <button className="checkout-btn" type="button" onClick={()=>props.handleSubmit()}>{props.text}</button>
+    <button className="checkout-btn" type="button" onClick={()=>props.handleSubmit(props.chosenItem)}>{props.text}</button>
   );
   
   export  default Checkout
